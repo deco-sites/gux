1f7e79011f454d475a8989b9250612952355e98c
Giovanny Lisboa <giovanny.lisboa@gux.cx>
2024-08-01 03:12:20 +0000

    Release staging environment with 7 changes by Giovanny Lisboa

--- sections/Header2.tsx
+++ sections/Header2.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function Section({ message = "Olá, somos a GUX!" }: Props) {
   return (
-    <div class="border border-[#3B3B3B] flex font-['IBM Plex Mono']">
+    <div class="border border-[#3B3B3B] flex">
       <div class="py-[21px] px-[32px] uppercase">{ message }</div>
       <div class="ml-auto px-[21.5px] py-[21px] border-l border-l-[#3B3B3B]">PT</div>
       <div class="px-[21.5px] py-[21px] border-l border-l-[#3B3B3B]">EN</div>
