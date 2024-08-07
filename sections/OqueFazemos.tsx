fbbcaceb780af85c40b4cfdfed4ef65122947f68
Giovanny Lisboa <162643577+glisboapuc@users.noreply.github.com>
2024-08-07 08:15:26 -0300

    inclusao da edicao do titulo da section

--- sections/OqueFazemos.tsx
+++ sections/OqueFazemos.tsx
@@ -11,72 +11,76 @@ interface Props {
     * @description Informe a categoria.
     */
     categorias?: Categoria[];
+    titulo?: string;
   }
   
-export default function Section({ categorias = [
-    {
-        nome: 'DESCOBERTA',
-        servicos: [
-            'Pesquisa ',
-            'Análise',
-            'Entrevistas',
-            'Benchmarking',
-            'Mapeamento de jornadas',
-            'Focus Group',
-            'Cliente Oculto',
-            'Estudo etnográfico',
-        ]
-    },
-    {
-        nome: 'DEFINIÇÃO',
-        servicos: [
-            'Estratégia de produto',
-            'Proposta de valor',
-            'Backlog',
-            'Roadmap',
-            'Conceituação',
-            'Testes de conceito',
-            'Testes de usabilidade'
-        ],
-    },
-    {
-        nome: 'DESENHO',
-        servicos: [
-            'Arquitetura de informação',
-            'Mapas',
-            'Fluxos',
-            'Jornadas',
-            'Identidade visual',
-            'Prototipagem',
-            'Design System',
-            'UX Writing',
-            'Motion Design',
-            'Sound Design',
-        ]
-    },
-    {
-        nome: 'DESENVOLVIMENTO',
-        servicos: [
-            'Tech Assessment',
-            'Integrações de sistema',
-            'Estratégia e implementação de LLM',
-            'Desenvolvimento full stack para dispositivos móveis e web',
-            'Arquitetura DevOps',
-            'Acessibilidade',
-            'Controle de Qualidade (QA)',
-        ]
-    }
-] }: Props) {
+export default function Section({
+    categorias = [
+        {
+            nome: 'DESCOBERTA',
+            servicos: [
+                'Pesquisa ',
+                'Análise',
+                'Entrevistas',
+                'Benchmarking',
+                'Mapeamento de jornadas',
+                'Focus Group',
+                'Cliente Oculto',
+                'Estudo etnográfico',
+            ]
+        },
+        {
+            nome: 'DEFINIÇÃO',
+            servicos: [
+                'Estratégia de produto',
+                'Proposta de valor',
+                'Backlog',
+                'Roadmap',
+                'Conceituação',
+                'Testes de conceito',
+                'Testes de usabilidade'
+            ],
+        },
+        {
+            nome: 'DESENHO',
+            servicos: [
+                'Arquitetura de informação',
+                'Mapas',
+                'Fluxos',
+                'Jornadas',
+                'Identidade visual',
+                'Prototipagem',
+                'Design System',
+                'UX Writing',
+                'Motion Design',
+                'Sound Design',
+            ]
+        },
+        {
+            nome: 'DESENVOLVIMENTO',
+            servicos: [
+                'Tech Assessment',
+                'Integrações de sistema',
+                'Estratégia e implementação de LLM',
+                'Desenvolvimento full stack para dispositivos móveis e web',
+                'Arquitetura DevOps',
+                'Acessibilidade',
+                'Controle de Qualidade (QA)',
+            ]
+        },
+    ],
+    titulo = 'O que fazemos'
+}: Props) {
     return (
         <section class="border border-t-0 border-[#3B3B3B]">
             <div class="flex flex-col md:flex-row">
                 <div class="flex-[1_1_auto] md:basis-1/5 px-8 pt-8 md:border-r border-[#3B3B3B] ibm-plex-mono-light">
-                    <p class="text-sm text-[#969696] uppercase">O que fazemos</p>
+                    <p class="text-sm text-[#969696] uppercase">{titulo}</p>
                 </div>
                 <div class="flex flex-wrap gap-[80px] md:gap-[32px] flex-[1_1_auto] md:basis-4/5 px-8 pt-5 md:pt-8 pb-[256px] kantumruy-pro-light">
                   {categorias.map((categoria) => (
                     <div class="flex flex-col sm:flex-row gap-[80px] sm:gap-[32px]">
-                      <div class="min-w-[202px]">
+                      <div class="min-w-[202px] w-fit">
                         <p class="text-base text-white uppercase mb-[20px]">{ categoria.nome }</p>
                         {categoria.servicos.map((servico) => (
                         <p class="text-sm font-normal text-[#969696] leading-[140%]">{ servico }</p>
