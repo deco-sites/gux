375b3579286c826ed90806977012cd8fbae041eb
Giovanny Lisboa <162643577+glisboapuc@users.noreply.github.com>
2024-08-07 14:30:57 -0300

    edicao de descricao com base em texto

--- sections/OqueFazemos.tsx
+++ sections/OqueFazemos.tsx
@@ -1,8 +1,8 @@
-export type Servico = string;
+import type { HTMLWidget } from "apps/admin/widgets.ts";
 
 export type Categoria = {
   nome: string;
-  servicos: Servico[];
+  descricao: HTMLWidget;
 };
 
 
@@ -18,55 +18,19 @@ export default function Section({
     categorias = [
         {
             nome: 'DESCOBERTA',
-            servicos: [
-                'Pesquisa ',
-                'Análise',
-                'Entrevistas',
-                'Benchmarking',
-                'Mapeamento de jornadas',
-                'Focus Group',
-                'Cliente Oculto',
-                'Estudo etnográfico',
-            ]
+            descricao: 'Pesquisa<br>Análise<br>Entrevistas<br>Benchmarking<br>Mapeamento de jornadas<br>Focus Group<br>Cliente Oculto<br>Estudo etnográfico'
         },
         {
             nome: 'DEFINIÇÃO',
-            servicos: [
-                'Estratégia de produto',
-                'Proposta de valor',
-                'Backlog',
-                'Roadmap',
-                'Conceituação',
-                'Testes de conceito',
-                'Testes de usabilidade'
-            ],
+            descricao: 'Estratégia de produto<br>Proposta de valor<br>Backlog<br>Roadmap<br>Conceituação<br>Testes de conceito<br>Testes de usabilidade'
         },
         {
             nome: 'DESENHO',
-            servicos: [
-                'Arquitetura de informação',
-                'Mapas',
-                'Fluxos',
-                'Jornadas',
-                'Identidade visual',
-                'Prototipagem',
-                'Design System',
-                'UX Writing',
-                'Motion Design',
-                'Sound Design',
-            ]
+            descricao: 'Arquitetura de informação<br>Mapas<br>Fluxos<br>Jornadas<br>Identidade visual<br>Prototipagem<br>Design System<br>UX Writing<br>Motion Design<br>Sound Design'
         },
         {
             nome: 'DESENVOLVIMENTO',
-            servicos: [
-                'Tech Assessment',
-                'Integrações de sistema',
-                'Estratégia e implementação de LLM',
-                'Desenvolvimento full stack para dispositivos móveis e web',
-                'Arquitetura DevOps',
-                'Acessibilidade',
-                'Controle de Qualidade (QA)',
-            ]
+            descricao: 'Tech Assessment<br>Integrações de sistema<br>Estratégia e implementação de LLM<br>Desenvolvimento full stack para dispositivos móveis e web<br>Arquitetura DevOps<br>Acessibilidade<br>Controle de Qualidade (QA)'
         },
     ],
     titulo = 'O que fazemos'
@@ -77,18 +41,13 @@ export default function Section({
                 <div class="flex-[1_1_auto] md:basis-1/5 px-8 pt-8 md:border-r border-[#3B3B3B] ibm-plex-mono-light">
                     <p class="text-sm text-[#969696] uppercase">{titulo}</p>
                 </div>
-                <div class="flex flex-wrap gap-[80px] md:gap-[32px] flex-[1_1_auto] md:basis-4/5 px-8 pt-5 md:pt-8 pb-[256px] kantumruy-pro-light">
+                <div class="flex flex-wrap gap-[80px] md:gap-[32px] md:basis-4/5 px-8 pt-5 md:pt-8 pb-[256px] kantumruy-pro-light">
                   {categorias.map((categoria) => (
-                    <div class="flex flex-col sm:flex-row gap-[80px] sm:gap-[32px]">
-                      <div class="min-w-[202px] w-fit">
-                        <p class="text-base text-white uppercase mb-[20px]">{ categoria.nome }</p>
-                        {categoria.servicos.map((servico) => (
-                        <p class="text-sm font-normal text-[#969696] leading-[140%]">{ servico }</p>
-                        ))}
-                      </div>
+                    <div class="w-full md:w-1/5">
+                        <p class="text-base text-white uppercase mb-5">{ categoria.nome }</p>
+                          <p class="text-sm font-normal text-[#969696] leading-[140%]" dangerouslySetInnerHTML={{ __html: categoria.descricao || "" }}></p>
                     </div>
                   ))}
-
                 </div>
             </div>
         </section>
